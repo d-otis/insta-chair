7bcac341d937ec941a92f367a7240c53cb0cc1dd
d-otis <dan@dan-foley.com>
2021-02-08 17:16:36 -0500

    add console.log of date on program launch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const getUnsoldChairs = require('./src/getUnsoldChairs')
 const sendEmail = require('./src/sendEmail')
 
 ;(async () => {
+  console.log(`starting INSTA-CHAIR @ ${new Date().toLocaleString()}`)
   const chairs = await getUnsoldChairs()
   sendEmail(chairs)
 })()
