ff3fff46926c3ae8adf729a1d786ad48dd519c2e
d-otis <dan@dan-foley.com>
2021-02-08 20:59:05 -0500

    require savedChairs JSON and persisChairs() in main index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const getChairs = require('./src/getChairs')
 const sendEmail = require('./src/sendEmail')
+const savedChairs = require('./src/savedPosts.json')
+const persistChairs = require('./src/persistChairs')
 
 ;(async () => {
   console.log(`starting INSTA-CHAIR @ ${new Date().toLocaleString()}`)
